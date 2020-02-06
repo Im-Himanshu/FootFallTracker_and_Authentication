f0019651ed3432aa683c5fe24899f07e20efad7d
ImHimanShoe <goyalhimanshu414@gmail.com>
2020-02-07 01:01:26 +0530

    working case in second part with prediction from the model

---
+++ userSideApplication/faceDetection/ReadMe.md
@@ -0,0 +1,19 @@
+
+1. updating the default angular cli to latest 8.3.1 --- to support the typescreipt 3.5.X
+npm install --save-dev @angular/cli@latest
+https://stackoverflow.com/questions/44525746/global-angular-cli-version-greater-than-local-version/44526528
+    for angular and type script data 
+    https://gist.github.com/LayZeeDK/c822cc812f75bb07b7c55d07ba2719b3
+
+2. it was still not supporting hard coding to remove the warning -- it may cause problem in later steps, keep an eye for it 
+    but this is necessary for new tensorFlow.js to work on :-
+    possibly have to update to unstable 9.0.0 for stable control for typescript -
+    See how : https://www.angularjswiki.com/angular/update-angular-cli-version-ng-update-to-latest-6-7-versions/
+
+    use this official step guide for that : https://update.angular.io/#8.1:9.0
+    or typescript 3.6.4|3.7.2 both should work  -- try updating them here only
+    how to supress the warning :
+    https://stackoverflow.com/questions/57216110/the-angular-compiler-requires-typescript-3-4-0-and-3-5-0-but-3-5-3-was-found
+
+
+3. Running npm i @tensorflow/tfjs
---
+++ userSideApplication/faceDetection/src/app/home/home.page.ts
@@ -0,0 +1,45 @@
+import { Prediction } from './../prediction';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as tf from '@tensorflow/tfjs';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  @ViewChild('video', {static:true}) video: ElementRef;
+  predictions: Prediction[];
+  model: any;
+  loading = true;
+  constructor() { }
+
+  async ngOnInit() {
+    console.log('loading mobilenet model...');
+    this.model = await mobilenet.load();
+    console.log('Sucessfully loaded model');
+    this.loading = false;
+
+    setInterval(async () => {
+      this.predictions = await this.model.classify(this.video.nativeElement);
+      await tf.nextFrame();
+    }, 3000);
+  }
+
+  async ngAfterViewInit() {
+    const vid = this.video.nativeElement;
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          vid.srcObject = stream;
+
+        })
+        .catch((err0r) => {
+          console.log('Something went wrong!');
+        });
+    }
+  }
+
+}
