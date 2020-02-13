b697e62e966c69e239250e4e70d43c65d0346a06
ImHimanShoe <goyalhimanshu414@gmail.com>
2020-02-13 13:05:52 +0530

    register user

--- userSideApplication/faceDetection/src/app/home/registered-user.service.ts
+++ userSideApplication/faceDetection/src/app/home/registered-user.service.ts
@@ -14,6 +14,9 @@ export class RegisteredUserService {
   labeledFaceDescriptors: any[];
   private faceMatcher: any;
   onTabChangeEvent: Subject<any> = new Subject();
+  maxDistanceAllowed = 0.6;
+  index = 0;
+  exsistingLabel = {};
 
   constructor(
     public loadingController: LoadingController,
@@ -27,21 +30,38 @@ export class RegisteredUserService {
   }
 
   async addLabeledData(name, descriptions) {
+    if (this.exsistingLabel[name]) {
+      this.presentToast("label already Exist!!");
+      return;
+    }
     const data = new faceapi.LabeledFaceDescriptors(name, descriptions);
+    this.exsistingLabel[name] = descriptions;
     this.labeledFaceDescriptors.push(data);
     this.faceMatcher = await new faceapi.FaceMatcher(
       this.labeledFaceDescriptors,
-      0.6
+      this.maxDistanceAllowed // greater than this will be rejected
     );
   }
 
+  addIfNotMatching(data, description) {
+    if (data.distance && data.distance > this.maxDistanceAllowed + 0.2) {
+      let descriptions = [description];
+      this.addLabeledData("unknown-" + this.index, descriptions); // if it is the first one add it as a entry
+      this.index++;
+    }
+  }
+
   async findMatcher(description) {
+    // if does not exsist start with unknown
     if (!this.faceMatcher) {
       let descriptions = [description]; // array of description
-      await this.addLabeledData("unknown-1", descriptions); // if it is the first one add it as a entry
+      await this.addLabeledData("unknown-" + this.index, descriptions); // if it is the first one add it as a entry
+      this.index = this.index + 1;
     }
 
-    return this.faceMatcher.findBestMatch(description); // everytime a new label is added this is retrained so no need to retrain this one
+    let matcher = this.faceMatcher.findBestMatch(description); // everytime a new label is added this is retrained so no need to retrain this one
+    this.addIfNotMatching(matcher, description);
+    return matcher;
   }
 
   async presentLoading(message: string) {
